041835e4f703d02120e0ef32fe28eedb24a6132a
nljacob <neehaljacob@gmail.com>
2018-04-19 09:17:34 -0700

    completed layout

---
+++ app/data/friends.js
@@ -0,0 +1,89 @@
+// alert("friends.js!")
+
+var friends = [
+{
+    name: "Rachel",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        5,
+        4,
+        1,
+        2,
+        4,
+        1,
+        2,
+        3,
+        1,
+        6
+    ]
+},
+
+{
+    name: "Ross",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        2,
+        1,
+        6,
+        4,
+        2,
+        8,
+        2,
+        1,
+        8,
+        7
+    ]
+},
+
+{
+    name: "Phoebe",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        1,
+        2,
+        3,
+        4,
+        6,
+        8,
+        9,
+        2,
+        1,
+        5
+    ]
+},
+
+{
+    name: "Joey",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        2,
+        1,
+        1,
+        3,
+        5,
+        6,
+        8,
+        2,
+        1,
+        9
+    ]
+},
+
+{
+    name: "Chandler",
+    photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    scores: [
+        1,
+        0,
+        3,
+        5,
+        7,
+        8,
+        9,
+        6,
+        5,
+        4
+    ]
+}
+
+]
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,1 @@
+// alert("apiroutes.js!")
